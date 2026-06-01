d96e078b7664b3a1b7f5d81d972c00d9792ccffd
Camilo Contreras <camil.contreras@duocuc.cl>
2026-05-31 23:35:30 -0400

    Creacion de microservicio Clientes

---
+++ ms-clientes/ms-clientes/src/test/java/com/Ferreteria/ms_clientes/MsClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ferreteria.ms_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
