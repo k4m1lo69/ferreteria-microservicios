0544294bfb4538981c40e0b09b4aa1f42e5a3f24
Camilo Contreras <camil.contreras@duocuc.cl>
2026-06-01 00:42:14 -0400

    Creacion de DTO RegisterRequest

--- ms-auth/ms-auth/src/main/java/com/Ferreteria/ms_auth/dto/RegisterRequest.java
+++ ms-auth/ms-auth/src/main/java/com/Ferreteria/ms_auth/dto/RegisterRequest.java
@@ -1,4 +1,18 @@
 package com.Ferreteria.ms_auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+
 public class RegisterRequest {
+
+    @NotBlank(message = "Username obligatorio")
+    private String username;
+
+    @NotBlank(message = "Password obligatorio")
+    private String password;
+
+    @NotBlank(message = "Rol obligatorio")
+    private String rol;
 }
