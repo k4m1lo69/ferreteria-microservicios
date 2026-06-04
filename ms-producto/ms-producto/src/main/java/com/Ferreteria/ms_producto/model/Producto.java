6f7969d5fe549a40b0214a2fcbae62eac5b5fc43
Camilo Contreras <camil.contreras@duocuc.cl>
2026-06-04 19:48:33 -0400

    Implementacion de model y Producto.java

---
+++ ms-producto/ms-producto/src/main/java/com/Ferreteria/ms_producto/model/Producto.java
@@ -0,0 +1,36 @@
+package com.Ferreteria.ms_producto.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "productos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = false, length = 255)
+    private String descripcion;
+
+    @Column(nullable = false)
+    private Double precio;
+
+    @Column(nullable = false, length = 50)
+    private String categoria;
+
+    @Column(nullable = false, length = 50)
+    private String marca;
+}
