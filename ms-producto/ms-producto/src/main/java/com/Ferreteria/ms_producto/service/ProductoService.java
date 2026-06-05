a7338188f4aea3ae689273151b8f8f5b348260e3
Camilo Contreras <camil.contreras@duocuc.cl>
2026-06-04 23:32:41 -0400

    Implementacion de service

--- ms-producto/ms-producto/src/main/java/com/Ferreteria/ms_producto/service/ProductoService.java
+++ ms-producto/ms-producto/src/main/java/com/Ferreteria/ms_producto/service/ProductoService.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductoService {
@@ -20,20 +21,75 @@ public class ProductoService {
         this.productoRepository = productoRepository;
     }
 
-    public List<ProductoDTO> save(ProductoDTO dto) {
+    public ProductoDTO save(ProductoDTO dto) {
         Producto entity = Producto.builder()
                 .nombre(dto.getNombre())
                 .descripcion(dto.getDescripcion())
                 .precio(dto.getPrecio())
                 .categoria(dto.getCategoria())
-                .marca((dto.getMarca()))
+                .marca(dto.getMarca())
                 .build();
         Producto saved = productoRepository.save(entity);
         log.info("Producto creado: {}", saved.getNombre());
         return convertirADTO(saved);
-
+    }
+    public List<ProductoDTO> getAll() {
+        log.info("consultando todos los productos");
+        return  productoRepository.findAll()
+                .stream()
+                .map(this::convertirADTO)
+                .collect(Collectors.toList());
     }
 
-    
+    public ProductoDTO getById(Long id) {
+        Producto producto = productoRepository.findById(id)
+                .orElseThrow(() ->{
+                    log.error("Producto no encontrado: {}", id);
+                    return new RuntimeException(
+                            "Producto con ID " + id + "no encontradp");
+                });
+        return convertirADTO(producto);
+    }
+
+    public ProductoDTO update(Long id, ProductoDTO dto) {
+        Producto producto = productoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(
+                        "Producto con ID " + id + "no encontrado"));
+        producto.setNombre(dto.getNombre());
+        producto.setDescripcion(dto.getDescripcion());
+        producto.setPrecio(dto.getPrecio());
+        producto.setMarca(dto.getMarca());
+        log.info("Producto eliminado: {}", id);
+        return convertirADTO(productoRepository.save(producto));
+    }
+
+    public boolean delete(Long id) {
+        if (productoRepository.existsById(id)) {
+            productoRepository.existsById(id);
+            log.info("Producto eliminado {}", id);
+            return true;
+        }
+        log.warn("Intento de eliminar producto inexistente: {}", id);
+        return false;
+    }
+
+    public List<ProductoDTO> getByCategoria(String categoria) {
+        return productoRepository.findByCategoria(categoria)
+                .stream()
+                .map(this::convertirADTO)
+                .collect(Collectors.toList());
+    }
+
+
+    private ProductoDTO convertirADTO(Producto producto) {
+        return ProductoDTO.builder()
+                .id(producto.getId())
+                .nombre(producto.getNombre())
+                .descripcion(producto.getDescripcion())
+                .precio(producto.getPrecio())
+                .categoria(producto.getCategoria())
+                .marca(producto.getMarca())
+                .build();
+    }
 
 }
