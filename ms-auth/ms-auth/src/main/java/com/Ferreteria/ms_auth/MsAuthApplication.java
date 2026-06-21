f4e7991dac51717cfc053b8e3738cca87fbdde10
Camilo Contreras <camil.contrerass@duocuc.cl>
2026-06-21 18:52:42 -0400

    feat: actualiza configuracion Gateway a nuevo formato YAML, corrige SecurityConfig de ms-auth

--- ms-auth/ms-auth/src/main/java/com/Ferreteria/ms_auth/MsAuthApplication.java
+++ ms-auth/ms-auth/src/main/java/com/Ferreteria/ms_auth/MsAuthApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsAuthApplication {
+public class
+
+MsAuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsAuthApplication.class, args);
--- ms-auth/ms-auth/src/main/java/com/Ferreteria/ms_auth/security/SecurityConfig.java
+++ ms-auth/ms-auth/src/main/java/com/Ferreteria/ms_auth/security/SecurityConfig.java
@@ -9,16 +9,17 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
-
 public class SecurityConfig {
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                    .anyRequest().permitAll()
+                        .anyRequest().permitAll()
                 );
+
         return http.build();
     }
 
