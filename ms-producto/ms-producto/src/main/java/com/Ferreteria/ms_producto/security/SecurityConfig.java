f5fc82fe4f75eb681fbe880a1df413fa001e2d63
Camilo Contreras <camil.contrerass@duocuc.cl>
2026-06-23 00:12:46 -0400

    fix: actualiza Gateway a host.docker.internal para Docker, corrige SecurityConfig de ms-producto

--- ms-producto/ms-producto/src/main/java/com/Ferreteria/ms_producto/security/SecurityConfig.java
+++ ms-producto/ms-producto/src/main/java/com/Ferreteria/ms_producto/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.Ferreteria.ms_producto.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,15 +20,31 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(s ->
-                        s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS))
+
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated()
+
+                        // CONSULTAS PÚBLICAS
+                        .requestMatchers(HttpMethod.GET,
+                                "/api/productos/**")
+                        .permitAll()
+
+                        // TODO LO DEMÁS REQUIERE JWT
+                        .anyRequest()
+                        .authenticated()
                 )
-                .addFilterBefore(jwtFilter,
-                        UsernamePasswordAuthenticationFilter.class);
+
+                .addFilterBefore(
+                        jwtFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
         return http.build();
     }
 }
